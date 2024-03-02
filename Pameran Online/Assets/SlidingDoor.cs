7fbf677d4e832a230dae0789ed3fcddba4d02144
Rajendra Abhinaya <abhinaya25152003@gmail.com>
2024-03-02 20:05:04 +0700

    Expo Hall WIP

---
+++ Pameran Online/Assets/SlidingDoor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoor : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    public Vector3 openPosition;
+    private bool isOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    void Update(){
+        if(isOpen){
+            transform.position = Vector3.Lerp(transform.position, openPosition, Time.deltaTime * 5);
+        }
+        else{
+            transform.position = Vector3.Lerp(transform.position, initialPosition, Time.deltaTime * 5);
+        }
+    }
+
+    public void Open(){
+        isOpen = true;
+    }
+
+    public void Close(){
+        isOpen = false;
+    }
+}
