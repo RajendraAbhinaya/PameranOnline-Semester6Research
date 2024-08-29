b60c3300027fd4c2da3e8993fd4864ce334984da
Rajendra Abhinaya <abhinaya25152003@gmail.com>
2024-08-29 15:49:01 +0700

    Gemini API

--- Pameran Online/Assets/Scripts/Gemini.cs
+++ Pameran Online/Assets/Scripts/Gemini.cs
@@ -9,38 +9,46 @@ using System.Threading.Tasks;
 
 public class Gemini : MonoBehaviour
 {
-    const string gasUrl = "https://script.google.com/macros/s/AKfycbzZt39wdJ8fP7wUk4pb7qW3XoA25XQm3c_AnCMwVNWWq4gtvWg0QQAD-TbZSY0eO-kC/exec";
+    const string gasUrl = "https://script.google.com/macros/s/AKfycbxzaYwRmU4v-icQZF8wvunbuOiS_-0aVur3IsBk00TZwxwbvkfl28BLmI9yaCjE87a0/exec";
 
-    private string inputPrompt;
+    private string inputPrompt = "What are the newest toyota releases in indonesia";
     private GeminiInputField geminiInputfield;
     List<GeminiContent> _chatHistory = new();
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //StartCoroutine(SendDataToGas());
     }
- 
-    async Task<string> OnChat(string text)
+
+    private IEnumerator SendDataToGas()
     {
-        _chatHistory.Add(GeminiContent.GetContent(text, GeminiRole.User));
-        GeminiChatResponse response = await GeminiManager.Instance.Request<GeminiChatResponse>(
-            new GeminiChatRequest(GeminiModel.Gemini1_5Flash)
-            {
-                Contents = _chatHistory.ToArray(),
-            }
-        );
+        Debug.Log("Running chat request.");
+        geminiInputfield.SetResponseText("Running chat request.");
         
-        _chatHistory.Add(response.Candidates[0].Content);
-        Debug.Log(response.Parts[0].Text);
-        return response.Parts[0].Text;
+        WWWForm form = new WWWForm();
+        form.AddField("parameter", inputPrompt);
+        UnityWebRequest www = UnityWebRequest.Post(gasUrl, form);
+
+        yield return www.SendWebRequest();
+        string response = "";
+
+        if(www. result == UnityWebRequest.Result.Success){
+            response = www.downloadHandler.text;
+        }
+        else{
+            response = "There was an error";
+        }
+        Debug.Log(response);
+        geminiInputfield.SetResponseText(response);
     }
 
     public void EnterPrompt(string prompt, GeminiInputField inputField)
     {
         inputPrompt = prompt;
         geminiInputfield = inputField;
-        RunChatRequest();
+        //RunChatRequest();
+        StartCoroutine(SendDataToGas());
     }
 
     private async void RunChatRequest()
@@ -49,7 +57,7 @@ public class Gemini : MonoBehaviour
         geminiInputfield.SetResponseText("Running chat request.");
     
         GeminiChatResponse response = await GeminiManager.Instance.Request<GeminiChatResponse>(
-            new GeminiChatRequest(GeminiModel.Gemini1_5Pro)
+            new GeminiChatRequest(GeminiModel.Gemini1_5Pro, true)
             {
                 Contents = new GeminiContent[]
                 {
